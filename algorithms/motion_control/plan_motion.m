a94708254ff457c4764598cc2ac54230cc046e34
Mandrakes83 <dany.sramek@gmail.com>
2026-04-18 16:57:33 +0200

    Motion Controller Adjusted and limited

--- algorithms/motion_control/plan_motion.m
+++ algorithms/motion_control/plan_motion.m
@@ -8,6 +8,8 @@ target = get_target(public_vars.estimated_pose, public_vars.path);
 
 % II. Compute motion vector
 
+L = read_only_vars.agent_drive.interwheel_dist;
+
 %Spocitat polohu P
 theta = public_vars.estimated_pose(3);
 epsilon = 0.05; %delka tycky
@@ -23,9 +25,29 @@ dyp = kappa*(target(2) - yp);
 v = dxp*cos(theta) + dyp*sin(theta);
 w = (-dxp*sin(theta) + dyp*cos(theta))/(epsilon);
 
-if(v < 0.2 && w < 0.1)
-    v = 0.2;
+% Limits
+v_max = 0.9; 
+v_min = 0.3;
+w_max = pi/16;
+w_deadzone = pi/16;
+
+
+% Limitacia uhlovej rychlosti robota
+if w < 0
+    w = max(w,-w_max);
+elseif(w > 0)
+    w = min(w,w_max);
 end
+
+% Minimalna rychlost robota
+if(abs(w) < w_deadzone) 
+    % Ak ide rovno exstuje min speed
+    v = max(min(v, v_max), v_min);
+else
+    % Ak chce rotovat existuje len max speed
+    v = min(v,v_max);
+end
+
 
 public_vars.motion_vector = [(2*v+w)/2,(2*v-w)/2]; %right,left
 end
--- algorithms/student_workspace.m
+++ algorithms/student_workspace.m
@@ -1,18 +1,18 @@
 function [public_vars] = student_workspace(read_only_vars,public_vars)
 %STUDENT_WORKSPACE Summary of this function goes here
 
-Init_cycle_count = 51;
+Init_cycle_count = 50;
 
 % 8. Perform initialization procedure
 if (read_only_vars.counter == 1)
           
-    public_vars = init_particle_filter(read_only_vars, public_vars);
+    % public_vars = init_particle_filter(read_only_vars, public_vars);
     public_vars = init_kalman_filter(read_only_vars, public_vars);
 
 end
 
 % 9. Update particle filter
-public_vars.particles = update_particle_filter(read_only_vars, public_vars);
+% public_vars.particles = update_particle_filter(read_only_vars, public_vars);
 
 % 10. Update Kalman filter
 if (read_only_vars.counter > Init_cycle_count)
@@ -27,9 +27,9 @@ end
 % 12. Path planning
 public_vars.path = plan_path(read_only_vars, public_vars);
 
-public_vars.path(1:100,:) = [linspace(2,10,100);linspace(2,8,100)]';
-public_vars.path(101:150,:) = [linspace(10,16,50);linspace(8,6,50)]';
-public_vars.path(151:200,:) = [linspace(16,16,50);linspace(6,2,50)]';
+public_vars.path(1:25,:) = [linspace(2.5,10,25);linspace(2,8,25)]';
+public_vars.path(26:50,:) = [linspace(10,16,25);linspace(8,6,25)]';
+public_vars.path(51:75,:) = [linspace(16,16,25);linspace(6,2,25)]';
 
 
 
@@ -47,13 +47,13 @@ elseif(read_only_vars.counter == Init_cycle_count-1)
 
     public_vars.sigma(1,1) = std(read_only_vars.gnss_history(:,1))^2;
     public_vars.sigma(2,2) = std(read_only_vars.gnss_history(:,2))^2;
-    public_vars.sigma(3,3) = (30*pi/180)^2;
+    public_vars.sigma(3,3) = (pi/8)^2;
 
     
     public_vars.kf.Q = diag([
-            0.02^2;                 % x process noise
+            0.005^2;                 % x process noise
             0.02^2;                 % y process noise
-            (2*pi/180)^2            % theta process noise
+            0.02^2            % theta process noise
         ]);
 
     public_vars.kf.R = cov(read_only_vars.gnss_history);
